ec0a9dc860d8e04a0c5bc0fd96e32c7966d7ec4d
toavinaraza <toavinaraza42@gmail.com>
2023-03-14 07:39:22 +0300

    changeSprint1

--- Sprint1/src/java/etu1911/framework/servlet/FrontServlet.java
+++ Sprint1/src/java/etu1911/framework/servlet/FrontServlet.java
@@ -2,16 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package controller;
+package etu1911.framework.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import utilitaires.Utilities;
+
 
 /**
  *
@@ -27,19 +26,12 @@ public class FrontServlet extends HttpServlet {
      *
      * @param request servlet request
      * @param response servlet response
-     * @param url 
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response,String url)
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String[] path=Utilities.getUrl(url);
-        PrintWriter out=response.getWriter();
-        out.println("The Url is:"+url);
-        out.println("The different part of the url:");
-        for (String string : path) {
-            out.println(string);
-        }
+       
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -54,7 +46,7 @@ public class FrontServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response,request.getServletPath());
+        processRequest(request, response);
     }
 
     /**
@@ -68,7 +60,7 @@ public class FrontServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response,request.getServletPath());
+        processRequest(request, response);
     }
 
     /**
